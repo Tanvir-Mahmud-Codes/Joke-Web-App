8615c906f4d7494a6e46f8488426c560895310b4
Tanvir-Mahmud-Codes <tanvir9345@gmail.com>
2025-02-09 13:44:05 +0600

    Add files via upload

---
+++ Models/Joke.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Principal;
+
+namespace JokeWebApp.Models
+{
+    public class Joke
+    {
+
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set;}
+
+        public Joke() {
+        
+        
+        }
+    }
+}
